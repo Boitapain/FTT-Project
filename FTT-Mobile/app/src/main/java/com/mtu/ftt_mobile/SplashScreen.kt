78cd9c2fc0a7c32d7a14bb77b387e695d689c520
Vincent Bullion <vincentbullion@icloud.com>
2022-11-09 13:49:49 +0000

    Added laucher icon and splash screen to the Mobile app

---
+++ FTT-Mobile/app/src/main/java/com/mtu/ftt_mobile/SplashScreen.kt
@@ -0,0 +1,14 @@
+package com.mtu.ftt_mobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+        finish()
+    }
+}
