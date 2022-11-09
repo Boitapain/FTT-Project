014c9e7aaf24f20af78b5ade89b4eae437e3c6f9
Vincent Bullion <vincentbullion@icloud.com>
2022-11-09 15:20:46 +0000

    Added Register and Login activity

--- FTT-Mobile/app/src/main/java/com/mtu/ftt_mobile/MainActivity.kt
+++ FTT-Mobile/app/src/main/java/com/mtu/ftt_mobile/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.mtu.ftt_mobile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(Manifest.permission.INTERNET),
+            0
+        )
+        mainActivityContext = this
+    }
+
+    fun login(v: View?) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivityForResult(intent, 2)
+    }
+
+    fun register(v: View?) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val responseText = findViewById<TextView>(R.id.responseText)
+        if (requestCode == 2) { // Login
+            responseText.text = "Successful Login."
+        } else {
+            responseText.text = "Invalid or no data entered. Please try again."
+        }
+    }
+
+    companion object {
+        var mainActivityContext: Context? = null
+        var postUrl = "http://192.168.16.102:5000"
     }
 }
