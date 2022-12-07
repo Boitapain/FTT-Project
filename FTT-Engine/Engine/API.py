d3429abe5c9f5adc2e59e85ddad0817bf451cc6a
Ethan Ponce <ethan.ponce81@gmail.com>
2022-12-07 16:00:41 +0000

    comment explaingin how to run the API

--- FTT-Engine/Engine/API.py
+++ FTT-Engine/Engine/API.py
@@ -102,3 +102,15 @@ def chatbot():
 
 app.run(host="127.0.0.1", port=5000, debug=True, threaded=True)
 
+"""
+ * Serving Flask app 'API' (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 873-873-224
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+ Run: python -m flask --app .\API.py run
+"""
